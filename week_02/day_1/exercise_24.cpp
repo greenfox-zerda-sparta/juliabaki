068bf5c57db423fb6be03ee0aced45222a69705f
Julia Baki <bakijulia@gmail.com>
2016-10-24 16:11:55 +0200

    Exercise 23-26 done.

---
+++ week_02/day_1/exercise_24.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : exercise_24.cpp
+// Author      : juliabaki
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int ae = 4;
+	string text = "Gold";
+	// print content of the text variable ae times
+	for (int i=1; i <= ae; i++){
+		cout << text;
+	}
+	return 0;
+}
