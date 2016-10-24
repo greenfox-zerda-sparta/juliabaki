fef1c997ea8f5ce0a24b81cf896b711d606c99ca
Julia Baki <bakijulia@gmail.com>
2016-10-24 12:52:59 +0200

    Exercise 9 done.

---
+++ week_02/day_1/exercise_9.cpp
@@ -0,0 +1,25 @@
+//============================================================================
+// Name        : exercise_9.cpp
+// Author      : juliabaki
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	int i = 1357988018575474;
+	// tell if it has 11 as a divisor
+
+	if (i % 11 == 0){
+		cout << "The number has 11 as a divisor.";
+	}else {
+		cout << "The number not has 11 as a divisor.";
+	}
+	return 0;
+}
