f21fa73182425b03d133646d404e8798e337c450
Julia Baki <bakijulia@gmail.com>
2016-10-25 11:56:25 +0200

    Exercise 1-8 done

---
+++ week_02/day_2/exercise_01.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : exercise_01.cpp
+// Author      : juliabaki
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int a[] = {4, 5, 6, 7};
+	// get the 3rd element of a
+
+	cout << a[2];
+
+  return 0;
+}
---
+++ week_02/day_2/exercise_02.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : exercise_02.cpp
+// Author      : juliabaki
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int b[] = {54, 23, 66, 12};
+	// add the second the third
+	cout << b[1]+b[2];
+  return 0;
+}
