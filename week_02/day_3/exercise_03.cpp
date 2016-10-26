eb44b3b9dd4b7668b0b1b38424e96a34e3f8413d
Julia Baki <bakijulia@gmail.com>
2016-10-26 13:21:26 +0200

    Exercise 1-8.

---
+++ week_02/day_3/exercise_03.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : exercise_03.cpp
+// Author      : juliabaki
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int number = 1234;
+  int* number_pointer = &number;
+  // update the value of number variable to 42 using the "number_pointer"
+  *number_pointer = 42;
+
+  cout << number;
+
+  return 0;
+}
---
+++ week_02/day_3/exercise_05.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : exercise_05.cpp
+// Author      : juliabaki
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int numbers[] = {5, 6, 7, 8, 9};
+  int* number_pointer = &numbers[0];
+  // The "number_pointer" should point to the third element of the array called "numbers",
+  // than please print its value with it. Solve the problem without reusiong the "&" operator
+  // for getting the address of the third element.
+
+  cout << number_pointer+2;
+
+  return 0;
+}
+
