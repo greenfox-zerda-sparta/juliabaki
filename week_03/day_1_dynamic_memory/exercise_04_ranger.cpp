5c09c3c31d57e904e7fa2975ba744bb35b6a6343
Julia Baki <bakijulia@gmail.com>
2016-11-02 22:52:38 +0100

    Reorganized and fixed exercise_04_ranger

--- week_03/day_1_dynamic_memory/exercise_04_ranger.cpp
+++ week_03/day_1_dynamic_memory/exercise_04_ranger.cpp
@@ -17,25 +17,28 @@
 #include <iostream>
 using namespace std;
 
-int length_caunter(int to, int from, int step){
-  int number_of_element;
-  if((to - from) % step == 0){
-      number_of_element = abs((to - from)/step);
-    } else {
-      number_of_element = abs((to - from)/step) + 1;
-    }
-  return number_of_element;
+int get_number_of_elements(int to, int from, int step) {
+  //int distance = to - from;
+  //return distance / step + (distance % step == 0 ? 0 : 1);
+  int number_of_elements;
+  if ((to - from) % step == 0) {
+    number_of_elements = abs((to - from) / step);
+  } else {
+    number_of_elements = abs((to - from) / step) + 1;
+  }
+  return number_of_elements;
 }
 
-int* range(int from, int to, int step){
-  int number_of_element = length_caunter(to, from, step);
-  int* new_array = new int[number_of_element];
-  for(int i = 0; i < number_of_element-1; i++){
+int* range(int from, int to, int step) {
+  int number_of_elements = get_number_of_elements(to, from, step);
+  int* new_array = new int[number_of_elements];
+  for (int i = 0; i < number_of_elements; i++) {
     new_array[i] = from + step * i;
     cout << new_array[i] << endl;
   }
   return new_array;
 }
+
 
 /**
  * Create a function called "range" that creates a new array and returns a pointer to it.
@@ -46,7 +49,7 @@ int* range(int from, int to, int step){
  * int step: the step between the numbers
  *
  * Examples:
- * range(0, 10, 2) -> {0, 2, 3, 4, 5, 6, 8}
+ * range(0, 10, 2) -> {0, 2, 4, 6, 8}
  * range(1, -8, -3) -> {1, -2, -5}
  *
  * It should delete any dynamically allocated resource before the program exits.
@@ -54,7 +57,8 @@ int* range(int from, int to, int step){
 
 int main() {
   int* new_array = range(-8, 2, 3);
-  delete[]new_array;
+  delete[] new_array;
+  new_array = nullptr;
 
   return 0;
 }
