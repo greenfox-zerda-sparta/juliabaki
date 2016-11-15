347c1b21c1b0d2fd5e03a4ffac4fa31ec8c5dfde
Julia Baki <bakijulia@gmail.com>
2016-11-15 13:07:42 +0100

    Exercise_05

---
+++ week_05/day_2_file_manipulation/ex_05.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  // Write a program that prints a content of a file called "fifth-exercise.txt"
+  // If it could not open the file it should write: "Could not open the file" to the standard error
+  // The program should return 2 if there where an error
+
+  ifstream my_file;
+  my_file.open("fifth-exercise.txt");
+
+  if (!my_file.is_open()) {
+    cerr << "Could not open the file";
+    return 2;
+   } else {
+     string file_content;
+     while(my_file >> file_content){
+       cout << file_content << endl;
+     }
+  }
+
+  my_file.close();
+
+  return 0;
+}
