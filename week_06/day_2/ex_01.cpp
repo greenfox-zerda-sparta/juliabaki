6cc7c0f87d5178ae362878abbbc0d47f933cf139
Julia Baki <bakijulia@gmail.com>
2016-11-22 11:14:12 +0100

    Exercise 01-03 done

---
+++ week_06/day_2/ex_01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// Write a try - catch block.
+// Throw an integer in the try block
+// Catch it in the catch block and write it out.
+
+int main() {
+  try{
+    throw 20;
+  }catch(int e){
+    cout << "This is: " << e << endl;
+  }
+
+  return 0;
+}
---
+++ week_06/day_2/ex_02.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+// Write the body of the function below.
+// It should throw an exception when b is zero.
+// Check the result.
+
+float division(int a, int b) throw (int) {
+  float result;
+  if(b == 0){
+      throw -1;
+  } else {
+    result = a / b;
+  }
+  return result;
+}
+
+int main() {
+  try {
+    cout << "Divison result: " << division(6, 2) << endl;
+    cout << "Division result: " << division(2, 0) << endl;
+  } catch (int excp) {
+    cout << "Exception: " << excp << endl;
+  }
+  return 0;
+}
