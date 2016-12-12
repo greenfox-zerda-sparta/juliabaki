4cb49fcc9457f7eaaa8c35da673d01557faa9205
Julia Baki <bakijulia@gmail.com>
2016-12-12 15:26:48 +0100

    Preprocessor Exercise 05

---
+++ week_08/day_1_preprocessor_directives/ex_05.cpp
@@ -0,0 +1,18 @@
+// Define a function like macro that prints out the parameter it gets,
+// and adds this string before it "DEBUG: " if __DEBUG is defined.
+// Otherwise adds "RELEASE: " before the string.
+// Use try it out. Illustrate that it works.
+
+#include <iostream>
+
+#ifdef __DEBUG
+#define get(a){std::cout << "Debug: " << #a << " = " << a;}
+#else
+#define get(a){ std::cout << "Release: " << #a << " = " << a;}
+#endif
+
+int main(){
+  int b = 3;
+  get(b);
+  return 0;
+}
