cf9c80a222bc0082e447fec0c987d49c4980ce5f
Julia Baki <bakijulia@gmail.com>
2016-12-12 12:35:48 +0100

    Preprocessor Exercise 01

---
+++ week_08/day_1_preprocessor_directives/ex_01.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#define INTEGER 3
+#define FLOAT 5.5678f
+#define STRING "alma"
+
+int main(){
+  std::cout << INTEGER << std::endl;
+  std::cout << FLOAT << std::endl;
+  std::cout << STRING << std::endl;
+
+  // Define 3 constants using a macro directive.
+  // One integer, one float and one string of characters.
+  // Print them out to the console in Your main function.
+
+  return 0;
+}
